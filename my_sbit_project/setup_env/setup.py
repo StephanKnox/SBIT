28838af6646221bb70ece732d770c2d34e9e6e65
StephanKnox <stanislav.i.kulchitskiy@gmail.com>
2025-08-16 14:09:42 +0200

    unit tests and base class

--- my_sbit_project/setup_env/setup.py
+++ my_sbit_project/setup_env/setup.py
@@ -1,27 +1,21 @@
-from pyspark.sql import SparkSession
-from my_sbit_project.utils.common import read_yaml, exec_sql
+from my_sbit_project.utils.common import DatabricksWorkflow, exec_sql
 
-class SetupHelper():   
-    def __init__(self, env, param):      
-        self.catalog = env
-        self.paramfile = param
+
+class SetupHelper(DatabricksWorkflow):   
+    def __init__(self, env, app_cfg):      
+        super.__init__(env, app_cfg)
+        #self.catalog = env
+        #self.paramfile = param
         self.initialized = False
-        self.app_name = 'SBIT'
-        self.spark = self.get_spark_session()
+        #self.app_name = 'SBIT'
+        #self.spark = self.get_spark_session()
 
-    def get_spark_session(self) -> SparkSession:
-        spark = (
-            SparkSession.builder
-            .appName(self.app_name )
-            .getOrCreate()
-        )
-        return spark
-
-    def run(self):
+    def launch(self):
         import time
         start = int(time.time())
-        cfg = read_yaml(self.paramfile)
-        tasks = cfg.get("job_config").get("tasks")
+
+        cfg = self.app_cfg.get("job_config")
+        tasks = cfg.get("tasks")
         print(f"\nStarting setup ...")
         
         for task_name, task_config in tasks.items():
--- my_sbit_project/utils/common.py
+++ my_sbit_project/utils/common.py
@@ -1,18 +1,53 @@
 import yaml
+from abc import ABC, abstractmethod
+from pyspark.sql import SparkSession
 from pyspark.sql.utils import AnalysisException
 
 
-def read_yaml(config_file):
-    """Read configuration yaml"""
-    with open(config_file, 'r') as cfg:
-        config = yaml.safe_load(cfg)
-    return config
+class DatabricksWorkflow(ABC):
+    def __init__(self, env, app_cgf):
+        self.env = env
+        self.app_cfg = self.read_yaml(app_cgf)
+        self.spark = self.get_spark_session()
+        #self.glb_cfg = glb_cfg
+        
+    def get_spark_session(self) -> SparkSession:
+        spark = (
+            SparkSession.builder
+            .appName(self.__class__.__name__)
+        )
+        if self.env == "LOCAL":
+        # For testing purposes only on local env
+            spark.master("local[*]")
+        
+        # Reading non-default spark session config options
+        ##for k, v in self.global_config.get("sparksession").get("config", {}).items():
+        ##    spark.config(k, v)
 
-def exec_sql(spark, sql_stmt, in_args=None):
-    """Execute provides sql statement with optional args"""
-    try:
-        spark.sql(sql_stmt, args=in_args)
-    except AnalysisException as e:
-        print("Analysis error:", e)
-    except Exception as e:
-        print("Unexpected error:", e)
+        return spark.getOrCreate()
+
+    def exec_sql(self, sql_stmt, in_args=None):
+        """Execute provided sql statement with optional args"""
+        try:
+            self.spark.sql(sql_stmt, args=in_args)
+        except AnalysisException as e:
+            # TO DO: replace prints with logging
+            print(f"Execution error when running SQL: {sql_stmt}")
+            raise 
+
+    @staticmethod
+    def read_yaml(cfg_file):
+        """Read configuration yaml"""
+        try:
+            with open(cfg_file) as cfg:
+                config = yaml.safe_load(cfg)    
+            return config
+        except FileNotFoundError as e:
+            # TO DO: replace prints with logging
+            print(f"""YAML file was not found at {cfg_file} or you dont have permissions
+                  to access it""")
+            raise
+        
+    @abstractmethod
+    def launch():
+        pass
